cfc2241626f3e3b6a2a366514043779212db95f2
chungchao.tsai <andy_20633@hotmail.com>
2023-01-06 16:05:03 +0800

    add cases

--- LeetCode/Solution.cs
+++ LeetCode/Solution.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace LeetCode
 {
+    /// <summary>
+    /// LeetCode solution, 
+    /// Runtime > 90%, ***
+    ///         > 70%, **
+    /// Memory  > 90%, ***
+    ///         > 70%, **
+    /// </summary>
     public class Solution
     {
         public Solution() { }
@@ -25,6 +32,11 @@ namespace LeetCode
             }
             return result;
         }
+        /// <summary>
+        /// R *, M *
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
         public bool PalindromeNumber(int num)
         {
             if (num < 0) return false;
@@ -39,6 +51,160 @@ namespace LeetCode
             }
 
             return ch.SequenceEqual(reversech);
+        }
+        /// <summary>
+        /// R *, M *
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public int RomanToInteger(string s)
+        {
+            Dictionary<string, int> romanNumbersDictionary = new()
+            {
+                { "I", 1 },
+                { "V", 5 },
+                { "X", 10 },
+                { "L", 50 },
+                { "C", 100 },
+                { "D", 500 },
+                { "M", 1000 },
+                { "IV", 4 },
+                { "IX", 9 },
+                { "XL", 40 },
+                { "XC", 90 },
+                { "CD", 400 },
+                { "CM", 900 },
+            };
+
+            int sum = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                var current = s[i].ToString();
+                romanNumbersDictionary.TryGetValue(current, out int num1);
+                if (i + 1 < s.Length)
+                {
+                    var twoChar = s[i].ToString() + s[i + 1].ToString();
+
+                    if (romanNumbersDictionary.TryGetValue(twoChar, out int num2))
+                    {
+                        sum += num2;
+                        i++;
+                    }
+                    else
+                    {
+                        sum += num1;
+                    }
+                }
+                else
+                {
+                    sum += num1;
+                }
+            }
+            return sum;
+        }
+        /// <summary>
+        /// R: *, M **
+        /// </summary>
+        /// <param name="strs"></param>
+        /// <param name="result"></param>
+        public string LongestCommonPrefix(string[] strs)
+        {
+
+            string prefix = "";
+            int prfixDigit = 0;
+            char[] s1Array = strs[0].ToCharArray();
+            try
+            {
+                while (true)
+                {
+                    if (s1Array.Length < prfixDigit + 1)
+                    {
+                        return prefix;
+                    }
+                    prefix += s1Array[prfixDigit];
+                    foreach (var str in strs)
+                    {
+                        if (prfixDigit > str.Length)
+                        {
+                            return prefix;
+                        }
+
+                        if (!str.StartsWith(prefix))
+                        {
+                            return prefix.Remove(prefix.Length - 1);
+                        }
+
+
+                    }
+                    prfixDigit++;
+                }
+
+            }
+            catch (Exception e)
+            {
+                return prefix;
+            }
+        }
+
+        public bool ValidParenthesesd(string s)
+        {
+            var result = true;
+            Dictionary<char, char> PairedParentheseHead = new Dictionary<char, char>();
+            PairedParentheseHead.Add('(', ')');
+            PairedParentheseHead.Add('[', ']');
+            PairedParentheseHead.Add('{', '}');
+            Dictionary<char, char> PairedParentheseTail = new Dictionary<char, char>();
+            PairedParentheseTail.Add(')', '(');
+            PairedParentheseTail.Add(']', '[');
+            PairedParentheseTail.Add('}', '{');
+            var charArray = s.ToCharArray();
+            char currentChar;
+            var ParenthesesAry = new List<char>();
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                currentChar = charArray[i];
+                var isFindHead = PairedParentheseHead.TryGetValue(currentChar, out char tail);
+                var isFindTail = PairedParentheseTail.TryGetValue(currentChar, out char head);
+
+                if (isFindHead) ParenthesesAry.Add(currentChar);
+                if (isFindTail)
+                {
+                    if (ParenthesesAry.Count == 0 || head != ParenthesesAry.Last()) return false;
+                    ParenthesesAry.RemoveAt(ParenthesesAry.Count - 1);
+                }
+
+            }
+            if (ParenthesesAry.Count() > 0) result = false;
+            return result;
+        }
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            ListNode result = new ListNode(0);
+            ListNode current = result;
+
+            if (l1 == null && l2 == null)
+                return null;
+            if (l1 == null)
+                return l2;
+            if (l2 == null)
+                return l1;
+
+            while (l1 != null && l2 != null)
+            {
+                if (l1.val <= l2.val)
+                {
+                    current.next = l1;
+                    l1 = l1.next;
+                }
+                else
+                {
+                    current.next = l2;
+                    l2 = l2.next;
+                }
+                current = current.next;
+            }
+            current.next = l1 ?? l2;
+            return result.next;
         }
     }
 }
--- LeetCodeTests/SolutionTests.cs
+++ LeetCodeTests/SolutionTests.cs
@@ -5,19 +5,19 @@ namespace LeetCodeTests
     [TestFixture]
     public class Tests
     {
-        private Solution s;
-        
+        private Solution sol;
+
         [SetUp]
         public void Setup()
         {
-            s = new Solution();
+            sol = new Solution();
         }
 
         [Test]
-        [TestCase(new int[] { 2,7,11,15 }, 9, new int[] { 0,1})]
+        [TestCase(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 })]
         public void TwoSum(int[] intArray, int target, int[] result)
         {
-            Assert.That(s.TwoSum(intArray, target), Is.EqualTo(result));
+            Assert.That(sol.TwoSum(intArray, target), Is.EqualTo(result));
         }
 
         [Test]
@@ -25,13 +25,66 @@ namespace LeetCodeTests
         [TestCase(-121, false)]
         [TestCase(0, true)]
         [TestCase(123, false)]
-        public void PalindromeNumber(int x, bool result )
+        public void PalindromeNumber(int x, bool result)
         {
-            Assert.That(s.PalindromeNumber(x), Is.EqualTo(result));
+            Assert.That(sol.PalindromeNumber(x), Is.EqualTo(result));
         }
 
+        [Test]
+        [TestCase("III", 3)]
+        [TestCase("LVIII", 58)]
+        [TestCase("LVIII", 58)]
+        [TestCase("MCMXCIV", 1994)]
+        public void RomanToInteger(string s, int result)
+        {
+            Assert.That(sol.RomanToInteger(s), Is.EqualTo(result));
+        }
 
+        [Test]
+        [TestCase(new string[] { "flower", "flow", "flight" }, "fl")]
+        [TestCase(new string[] { "dog", "racecar", "car" }, "")]
+        public void LongestCommonPrefix(string[] strs, string result)
+        {
+            Assert.That(sol.LongestCommonPrefix(strs), Is.EqualTo(result));
+        }
+
+        [Test]
+        [TestCase("()",true)]
+        [TestCase("()[]{}", true)]
+        [TestCase("(]", false)]
+        public void LongestCommonPrefix(string s, bool result)
+        {
+            Assert.That(sol.ValidParenthesesd(s), Is.EqualTo(result));
+        }
+
+        [Test]
+        public void MergeTwoLists()
+        {
+            ListNode list1_3 = new ListNode() { val = 33 };
+            ListNode list1_2 = new ListNode() { val = 22, next = list1_3 };
+            ListNode list1_1 = new ListNode() { val = 11, next = list1_2 };
+            ListNode list2_3 = new ListNode() { val = 35 };
+            ListNode list2_2 = new ListNode() { val = 25, next = list2_3 };
+            ListNode list2_1 = new ListNode() { val = 15, next = list2_2 };
+            var reulst1 = sol.MergeTwoLists(list1_1, list2_1);
+            Assert.That(reulst1.val, Is.EqualTo(11));
+            Assert.That(reulst1.next.val, Is.EqualTo(15));
+            Assert.That(reulst1.next.next.val, Is.EqualTo(22));
+            Assert.That(reulst1.next.next.next.val, Is.EqualTo(25));
+            Assert.That(reulst1.next.next.next.next.val, Is.EqualTo(33));
+            Assert.That(reulst1.next.next.next.next.next.val, Is.EqualTo(35));
+
+            ListNode list2_2_1 = null;
+            ListNode list2_2_2 = new ListNode() { val = 0 };
+            var result2 = sol.MergeTwoLists(list2_2_1, list2_2_2);
+
+            Assert.That(result2.val, Is.EqualTo(0));
+            Assert.That(result2.next,Is.Null);
+
+        }
     }
 
-    
+
 }
+
+    
